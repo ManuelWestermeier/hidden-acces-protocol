5c8b1b31e68c8e141a304e4f1c90b10dce69869c
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-03-09 21:37:41 +0100

    Add files via upload

---
+++ hap-client.js
@@ -0,0 +1,69 @@
+const { log } = require("console");
+const crypto = require("crypto")
+const net = require("net")
+
+const Handler = ({
+    send,
+    close,
+    onmsg,
+    socket
+}) => false
+
+async function createConection(options, handler = Handler) {
+
+    const socket = net.createConnection(options)
+
+    const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+        modulusLength: 2048,
+    });
+
+    var serverPublicKey = false
+
+    socket.write(`handshake hap 1.0\n${publicKey.export({ type: "pkcs1", format: "pem" }).toString("ascii")}`)
+
+    socket.on("data", chunk => {
+
+        const data = chunk.toString("utf-8")
+
+        if (!serverPublicKey) {
+
+            const lines = data.split("\n")
+
+            if (lines[0] == "handshake hap 1.0") {
+
+                serverPublicKey = lines.slice(1, lines.length).join("\n")
+
+                handler({
+                    send: (data) => {
+
+                        socket.write(crypto.publicEncrypt(serverPublicKey, data))
+
+                    },
+                    close: c => socket.destroy(c),
+                    onmsg: listener => socket.on("data", chunk => {
+
+                        listener(crypto.privateDecrypt(privateKey, chunk))
+
+                    }),
+                    socket
+                })
+
+            }
+
+            else socket.destroy(11)
+
+        }
+
+    })
+
+}
+
+createConection({ port: 2112, host: "localhost" }, ({ send, onmsg }) => {
+
+    send(Buffer.from("Hallo Welt"))
+
+    onmsg(data => {
+        log(data.toString("utf-8"))
+    })
+
+})
---
+++ hap-server.js
@@ -0,0 +1,69 @@
+const net = require("net")
+const crypto = require("crypto");
+const { log } = require("console");
+
+const Handler = ({
+    send,
+    close,
+    onmsg,
+    socket
+}) => false
+
+function createServer(port, handler = Handler) {
+    net.createServer(socket => {
+
+        const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+            modulusLength: 2048,
+        });
+
+        var clientPublicKey = false
+
+        socket.write(`handshake hap 1.0\n${publicKey.export({ type: "pkcs1", format: "pem" }).toString("ascii")}`)
+
+        socket.on("data", chunk => {
+
+            const data = chunk.toString("utf-8")
+
+            if (!clientPublicKey) {
+
+                const lines = data.split("\n")
+
+                if (lines[0] == "handshake hap 1.0") {
+
+                    clientPublicKey = lines.slice(1, lines.length).join("\n")
+
+                    handler({
+                        send: (data) => {
+
+                            socket.write(crypto.publicEncrypt(clientPublicKey, data))
+
+                        },
+                        close: c => socket.destroy(c),
+                        onmsg: listener => socket.on("data", chunk => {
+
+                            listener(crypto.privateDecrypt(privateKey, chunk))
+
+                        }),
+                        socket
+                    })
+
+                }
+
+                else socket.destroy(11)
+
+            }
+
+        })
+
+    }).listen(port)
+}
+
+createServer(2112, ({ send, onmsg }) => {
+
+    send(Buffer.from("Hallo Welt"))
+
+    onmsg(data => {
+        log(data.toString("utf-8"))
+    })
+
+})
